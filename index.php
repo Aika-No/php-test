42f817f5115d727066490731d3a34f8989f5d384
Aika-No <92627687+Aika-No@users.noreply.github.com>
2021-11-01 08:13:34 +0900

    q2

--- index.php
+++ index.php
@@ -24,4 +24,14 @@
     }
     echo '<br><br>';
 
-    
+    for ($i=1; $i<=10000; $i++){
+        $point = 0;
+        for ($j = 1; $j<=$i; $j++){
+            if ($i % $j == 0){
+                $point += 1;
+            }
+        }
+        if ($point ==2){
+            echo $i."\n";
+        }
+    }
