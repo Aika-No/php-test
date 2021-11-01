0569735b55afc6dc0c15bb8d3b29b6cfa24380ff
Aika-No <92627687+Aika-No@users.noreply.github.com>
2021-11-02 08:33:09 +0900

    q3

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// question 1
     function fizzBuzz ($number){
         switch (true){
             case $number % 15 === 0:
@@ -22,8 +23,10 @@
     for ($i=1; $i<=100; $i++){
         echo fizzBuzz($i). '';
     }
+
     echo '<br><br>';
 
+    // question 2
     for ($i=1; $i<=10000; $i++){
         $point = 0;
         for ($j = 1; $j<=$i; $j++){
@@ -35,3 +38,22 @@
             echo $i."\n";
         }
     }
+
+    echo '<br><br>';
+
+    // question 3
+    // country, state, prefecture, city, zipcode
+    $address = ['Japan', 'kinki', 'Osaka', 'Yao', '5810018'];
+    var_dump($address);
+    echo '<br>';
+    echo $address[0];
+    echo '<br>';
+    echo $address[3];
+    
+    echo '<br><br>';
+
+    $adress2= ['country'=>'Japan', 'state'=>'kinki', 'prefecture'=>'Osaka', 'city'=>'Yao', 'zip'=>'5810018'];
+    var_dump($adress2);
+    echo '<br>';
+    echo $adress2['country'].'<br>';
+    echo $adress2['zip'].'<br>';
